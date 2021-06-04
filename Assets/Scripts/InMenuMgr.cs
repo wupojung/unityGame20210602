ab5e81d601ce9fc3ed0ca2ac38f1ba3170243af8
Po Jung Wu <wupojung@gmail.com>
2021-06-04 19:22:16 +0800

    新增跳躍場景的code  (但是BuildSetting 沒有被git管理到) 需要自行在BuildSetting內重新加入場景，才可以用

--- Assets/Scripts/InMenuMgr.cs
+++ Assets/Scripts/InMenuMgr.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class InMenuMgr : MonoBehaviour
@@ -21,6 +22,6 @@ public class InMenuMgr : MonoBehaviour
 
     void OnClick()
     {
-        Debug.Log("aaaa");
+        SceneManager.LoadScene("InGame");
     }
 }
