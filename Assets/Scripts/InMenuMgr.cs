a7a057c3c03af00ee68d62976a058f60d780efbf
Po Jung Wu <wupojung@gmail.com>
2021-06-04 19:14:29 +0800

    新增按鈕 以及 Mgr 程式

---
+++ Assets/Scripts/InMenuMgr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InMenuMgr : MonoBehaviour
+{
+    public Button btnNext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnNext.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+        Debug.Log("aaaa");
+    }
+}
